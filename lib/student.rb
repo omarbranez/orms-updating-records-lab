b23331c52bfb0f98acab3c10459bfa32767379f3
Omar Branez <omar.branez@gmail.com>
2021-04-05 17:25:18 -0400

    commit tree 47ffdee1c6a393a2292b0ef6dacc78d844415f38

--- lib/student.rb
+++ lib/student.rb
@@ -4,6 +4,77 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
+  attr_accessor :name, :grade, :id
+  def initialize(id = nil, name, grade)
+    @id = id
+    @name = name
+    @grade = grade
+  end
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade INTEGER
+      )
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE IF EXISTS students
+    SQL
+    DB[:conn].execute(sql)
+  end #make sure it exists first, same as create table. if it's one line, just use sql =
+
+  def save
+    if @id
+      self.update #update is the same thing, just without creating a new @id
+    else
+      sql = <<-SQL
+        INSERT INTO students (name, grade)
+        VALUES (?, ?)
+      SQL
+      DB[:conn].execute(sql, @name, @grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
+  end
+
+  def self.create(name, grade)
+    student = Student.new(name, grade)
+    student.save #implicit student
+    #student
+  end
+
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    grade = row[2]
+    self.new(id, name, grade)
+  end    
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = ?
+    SQL
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
+  def update
+    sql = <<-SQL
+      UPDATE students
+      SET name = ?, grade = ?
+      WHERE id = ?
+    SQL
+    DB[:conn].execute(sql, @name, @grade, @id) #we change it in Student BEFORE we change it in the students.db
+  end
+
 
 
 end
